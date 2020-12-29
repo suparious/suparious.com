282d3a2e8c344142f0aa4df2cba7db6c9f24f4f0
Shaun Prince <shaun@techfusion.ca>
2020-12-29 13:03:32 -0800

    Update ubuntu18.04-cuda.sh

--- apps/idlekeeper/server-setup/ubuntu18.04-cuda.sh
+++ apps/idlekeeper/server-setup/ubuntu18.04-cuda.sh
@@ -1,4 +1,4 @@
-## Ubuntu 20.04
+## Ubuntu 18.04
 sudo apt update
 sudo apt-get install \
     git \
@@ -12,13 +12,21 @@ sudo apt-get install \
     linux-headers-$(uname -r) \
     build-essential
 
-# cuda toolkit
+# Shell customizations
+echo "rm -rf ~/.bash_history" >> .bashrc
+
+# Nvidia cuda toolkit and GPU drivers
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
 sudo apt-get -y install cuda
+
+## Alternative method, for drivers-only install
+#wget https://us.download.nvidia.com/tesla/450.80.02/NVIDIA-Linux-x86_64-450.80.02.run
+#chmod +x NVIDIA-Linux-x86_64-450.80.02.run
+#./NVIDIA-Linux-x86_64-450.80.02.run
 
 reboot
 
@@ -33,12 +41,13 @@ sudo apt-get update
 sudo apt-get install \
     docker-ce docker-ce-cli containerd.io
 
-#sudo systemctl status docker
-
 # enable current user access
 sudo usermod -aG docker ${USER}
-#exit
-#docker ps
+sudo su - ${USER}
+
+# test docker
+#sudo systemctl status docker
+docker ps
 
 # nvidia-docker2
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
@@ -57,10 +66,17 @@ sudo systemctl start docker
 sudo docker run --rm --gpus all nvidia/cuda:11.1-base nvidia-smi
 
 # build
-wget https://s3-us-west-2.amazonaws.com/suparious.com-git/suparious.com-master.zip
-unzip suparious.com-master.zip
-cd suparious.com-master/apps/idlekeeper/
-chmod +x build.sh && ./build.sh
+wget https://s3-us-west-2.amazonaws.com/suparious.com-git/suparious.com-master.zip && \
+unzip suparious.com-master.zip && \
+cd suparious.com-master/apps/idlekeeper/ && \
+chmod +x base-image/build.sh && \
+chmod +x build.sh && \
+./build.sh
+
+# clean-up
+cd ~
+rm -rf suparious.com-master*
+exit
 
 # run
 screen
