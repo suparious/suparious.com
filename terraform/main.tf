38ec04ba5a1ac0d81cf0fe8b69b6c2bcd45da4b8
BuildTools <unconfigured@null.spigotmc.org>
2021-01-01 22:54:17 -0800

    something fucked-up

--- terraform/main.tf
+++ terraform/main.tf
@@ -126,110 +126,111 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
   comment = "Suparious Services ${var.env}"
 }
 
-## Website ditribution layer
-#resource "aws_cloudfront_distribution" "s3_distribution" {
-#  origin {
-#    domain_name = module.main_s3.s3_bucket_regional_domain_name
-#    origin_id   = local.s3_origin_id
-#
-#    s3_origin_config {
-#      origin_access_identity = "origin-access-identity/cloudfront/${aws_cloudfront_origin_access_identity.origin_access_identity.id}" ####WHUT
-#    }
-#  }
-#
-#  enabled             = true
-#  is_ipv6_enabled     = true
-#  comment             = "Suparious Services website"
-#  default_root_object = "index.html"
-#
-#  logging_config {
-#    include_cookies = false
-#    bucket          = module.logging_s3.s3_bucket_regional_domain_name
-#    prefix          = "cloudfront"
-#  }
-#
-#  aliases = [var.domain_name, "*.${var.domain_name}"]
-#
-#  default_cache_behavior {
-#    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-#    cached_methods   = ["GET", "HEAD"]
-#    target_origin_id = local.s3_origin_id
-#
-#    forwarded_values {
-#      query_string = false
-#
-#      cookies {
-#        forward = "none"
-#      }
-#    }
-#
-#    viewer_protocol_policy = "allow-all"
-#    min_ttl                = 0
-#    default_ttl            = 3600
-#    max_ttl                = 86400
-#  }
-#
-#  # Cache behavior with precedence 0
-#  ordered_cache_behavior {
-#    path_pattern     = "/content/immutable/*"
-#    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-#    target_origin_id = local.s3_origin_id
-#
-#    forwarded_values {
-#      query_string = false
-#      headers      = ["Origin"]
-#
-#      cookies {
-#        forward = "none"
-#      }
-#    }
-#
-#    min_ttl                = 0
-#    default_ttl            = 86400
-#    max_ttl                = 31536000
-#    compress               = true
-#    viewer_protocol_policy = "redirect-to-https"
-#  }
-#
-#  # Cache behavior with precedence 1
-#  ordered_cache_behavior {
-#    path_pattern     = "/content/*"
-#    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#    cached_methods   = ["GET", "HEAD"]
-#    target_origin_id = local.s3_origin_id
-#
-#    forwarded_values {
-#      query_string = false
-#
-#      cookies {
-#        forward = "none"
-#      }
-#    }
-#
-#    min_ttl                = 0
-#    default_ttl            = 3600
-#    max_ttl                = 86400
-#    compress               = true
-#    viewer_protocol_policy = "redirect-to-https"
-#  }
-#
-#  price_class = "PriceClass_200"
-#
-#  restrictions {
-#    geo_restriction {
-#      restriction_type = "whitelist"
-#      locations        = ["US", "CA", "GB", "DE"]
-#    }
-#  }
-#
-#  tags = merge(local.common_tags, map(
-#    "Name", "${var.project}-${var.env}-dist"
-#  ))
-#
-#  viewer_certificate {
-#    acm_certificate_arn            = aws_acm_certificate.cert.arn
-#    ssl_support_method             = sni-only
-#    minimum_protocol_version       = "TLSv1"
-#  }
-#}
+# Website ditribution layer
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = module.main_s3.s3_bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = "origin-access-identity/cloudfront/${aws_cloudfront_origin_access_identity.origin_access_identity.id}" ####WHUT
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Suparious Services website"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = module.logging_s3.s3_bucket_regional_domain_name
+    prefix          = "cloudfront"
+  }
+
+  aliases = [var.domain_name, "*.${var.domain_name}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags = merge(local.common_tags, map(
+    "Name", "${var.project}-${var.env}-dist"
+  ))
+
+  viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate.cert.arn
+    cloudfront_default_certificate = false
+    minimum_protocol_version       = "TLSv1"
+    ssl_support_method             = "sni-only"
+  }
+}
