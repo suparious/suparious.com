7d8ef4eb0db6a79660c008adb7618e7de718c016
Shaun Prince <shaun@techfusion.ca>
2021-01-02 17:47:09 -0800

    updating deploy config to have apps zipped

--- ci/ci-deploy.sh
+++ ci/ci-deploy.sh
@@ -11,27 +11,23 @@ echo "Updating static web content"
 #aws s3 mb s3://${CI_BRANCH}.${DOMAIN} - no, use terraform
 echo "${CI_COMMIT_DESCRIPTION}-${CI_BRANCH}" > web/build.txt
 cat web/build.txt
-cd apps && zip -9 idlekeeper.zip idlekeeper && cd ..
+cd apps && zip -r9 idlekeeper.zip idlekeeper/ && cd ..
 case ${CI_BRANCH} in
   dev|stg)
     echo "Development env scripts"
-    aws s3 cp --acl public-read web/shit.html s3://${CI_BRANCH}.${DOMAIN}
-    aws s3 cp --acl public-read web/index.html s3://${CI_BRANCH}.${DOMAIN}
-    aws s3 cp --acl public-read web/build.txt s3://${CI_BRANCH}.${DOMAIN}
-    aws s3 cp --acl public-read apps/idlekeeper.zip s3://${CI_BRANCH}.${DOMAIN}
-    #aws s3 website s3://${CI_BRANCH}.${DOMAIN} --index-document index.html
-    #aws s3 website s3://${CI_BRANCH}.${DOMAIN} --error-document shit.html
-    #aws s3 sync --delete --acl public-read web s3://${CI_BRANCH}.${DOMAIN}
+    aws s3 sync --delete --acl public-read web s3://${CI_BRANCH}.${DOMAIN}
+    #aws s3 cp --acl public-read web/shit.html s3://${CI_BRANCH}.${DOMAIN}
+    #aws s3 cp --acl public-read web/index.html s3://${CI_BRANCH}.${DOMAIN}
+    #aws s3 cp --acl public-read web/build.txt s3://${CI_BRANCH}.${DOMAIN}
+    #aws s3 cp --acl public-read apps/idlekeeper.zip s3://${CI_BRANCH}.${DOMAIN}
     ;;
   prd)
     echo "Production deployment scripts"
+    #aws s3 sync --delete --acl public-read web s3://${DOMAIN}
     aws s3 cp --acl public-read web/shit.html s3://${DOMAIN}
     aws s3 cp --acl public-read web/index.html s3://${DOMAIN}
     aws s3 cp --acl public-read web/build.txt s3://${DOMAIN}
     aws s3 cp --acl public-read apps/idlekeeper.zip s3://${DOMAIN}
-    #aws s3 website s3://${DOMAIN} --index-document index.html
-    #aws s3 website s3://${DOMAIN} --error-document shit.html
-    #aws s3 sync --delete --acl public-read web s3://${DOMAIN}
     ;;
 esac
 echo "= END web:${CI_BRANCH}"
