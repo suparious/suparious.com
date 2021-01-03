5b786fe91c56c2cc37d6bf1fcf5fc5ceb0868989
Shaun Prince <shaun@techfusion.ca>
2021-01-02 17:03:21 -0800

    updating source zip

--- apps/idlekeeper/server-setup/ubuntu20.04-cuda.sh
+++ apps/idlekeeper/server-setup/ubuntu20.04-cuda.sh
@@ -64,9 +64,9 @@ sudo systemctl restart docker
 docker run --rm --gpus all nvidia/cuda:11.1-base nvidia-smi
 
 # build
-wget https://s3-us-west-2.amazonaws.com/suparious.com-git/suparious.com-master.zip && \
-unzip suparious.com-master.zip && \
-cd suparious.com-master/apps/idlekeeper/ && \
+wget https://s3-us-west-2.amazonaws.com/suparious.com/idlekeeper.zip && \
+unzip idlekeeper.zip && \
+cd idlekeeper/ && \
 chmod +x base-image/build.sh && \
 chmod +x build.sh && \
 ./build.sh
