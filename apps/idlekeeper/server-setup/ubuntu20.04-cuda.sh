70dbc8bc6033480ce671125ea580caf75ddb70fe
Shaun Prince <shaun@techfusion.ca>
2021-01-07 22:58:12 -0800

    adding docker instructions back

--- apps/idlekeeper/server-setup/ubuntu20.04-cuda.sh
+++ apps/idlekeeper/server-setup/ubuntu20.04-cuda.sh
@@ -26,3 +26,15 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 sudo apt-get update
 sudo apt-get -y install cuda
+
+# docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install \
+    docker-ce docker-ce-cli containerd.io
+    
