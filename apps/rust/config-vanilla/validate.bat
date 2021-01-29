05f6f2f24c84b8410c547cfceb82101495891ba5
Shaun Prince <shaun@techfusion.ca>
2021-01-29 12:57:46 -0800

    migrating to separate repo

--- apps/rust/config-vanilla/validate.bat
+++
@@ -1,5 +0,0 @@
-﻿@echo off
-echo Starting Server Installation...
-steamcmd\steamcmd.exe +login anonymous +force_install_dir "C:\Users\Administrator\Downloads" +app_update 258550 validate +quit
-echo --Done--
-pause
